ae9c690db4ecb40d625f587b7123d99e4754e1da
RusherRG <rushang101@gmail.com>
2019-08-08 14:42:50 +0530

    Updating

--- static/js/login.js
+++ static/js/login.js
@@ -8,3 +8,28 @@ function switch_tab(tab) {
         document.getElementById('register-div').style.display = 'flex';
     }
 }
+
+function login() {
+    email = document.getElementById('email').value;
+    password = document.getElementById('password').value;
+    data = {
+        'email': email,
+        'password': password,
+    }
+    console.log(data);
+}
+
+function register() {
+    name = document.getElementById('name').value;
+    email = document.getElementById('email').value;
+    phone = document.getElementById('phone').value;
+    password = document.getElementById('password').value;
+    
+    data = {
+        'name': name,
+        'email': email,
+        'phone': phone,
+        'password': password,
+    }
+    console.log(data);
+}
