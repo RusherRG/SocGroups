e37c47eb866033808281d0c9365499b414308be1
NetraGhaisas <netraghaisas@gmail.com>
2019-11-14 00:47:31 +0530

    ALter sequence

--- php/config.php
+++ php/config.php
@@ -9,6 +9,21 @@
         echo $e . ":(";
         exit;
     }
+    try{
+    $v_q="SELECT setval(pg_get_serial_sequence('visitor', 'visitor_id'), max(visitor_id)) FROM visitor;";
+    $m_q="SELECT setval(pg_get_serial_sequence('member', 'member_id'), max(member_id)) FROM member;";
+    $n_q="SELECT setval(pg_get_serial_sequence('notice', 'notice_id'), max(notice_id)) FROM notice;";
+    $s_q="SELECT setval(pg_get_serial_sequence('society', 'society_id'), max(society_id)) FROM society;";
+    $v_r=pg_query($v_q);
+    $m_r=pg_query($m_q);
+    $n_r=pg_query($n_q);
+    $s_r=pg_query($s_q);
+    // print_r($v_r);
+    }catch(Exception $e){
+        echo $e . ":(";
+        exit;
+    }
+
 
     $codepath="http://localhost/SocGroups/templates/";
     $phppath="http://localhost/SocGroups/php/";
